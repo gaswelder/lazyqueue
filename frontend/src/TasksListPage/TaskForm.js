c78cca1cedf2a13b8a85ad767ffdc1963d1f0dd4
gasweld <gasweld@gmail.com>
2020-01-03 21:33:26 +0300

    Style fixes

--- frontend/src/TasksListPage/TaskForm.js
+++ frontend/src/TasksListPage/TaskForm.js
@@ -1,5 +1,41 @@
 import React from "react";
-import classes from "./TaskForm.css";
+import styled from "styled-components";
+
+const Form = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+
+	& input,
+	textarea {
+		margin-bottom: 1em;
+	}
+
+	& textarea {
+		height: 3em;
+	}
+
+	& > div {
+		display: flex;
+		flex-direction: column;
+	}
+	& > div:nth-child(1) {
+		flex: 1;
+	}
+	& > div:nth-child(2) {
+		width: 100px;
+		flex: 0;
+	}
+	& > div:nth-child(3) {
+		flex: 1;
+		flex-basis: 100%;
+	}
+	& > div:nth-child(4) {
+		flex-direction: row;
+		flex: 1;
+		justify-content: flex-end;
+	}
+`;
 
 class TaskForm extends React.Component {
 	constructor() {
@@ -21,40 +57,45 @@ class TaskForm extends React.Component {
 		const { task = {}, onCancel, tags } = this.props;
 
 		return (
-			<form onSubmit={this.handleSubmit} className={classes.form}>
-				<label>Name</label>
-				<input
-					name="name"
-					required
-					defaultValue={task.name}
-					autoFocus
-					autoComplete="off"
-				/>
+			<Form onSubmit={this.handleSubmit}>
+				<div>
+					<label>Name</label>
+					<input
+						name="name"
+						required
+						defaultValue={task.name}
+						autoFocus
+						autoComplete="off"
+					/>
+				</div>
 
-				<label>Tag</label>
-				<input
-					name="tag"
-					defaultValue={task.tag}
-					autoComplete="off"
-					list="tags"
-				/>
+				<div>
+					<label>Tag</label>
+					<input
+						name="tag"
+						defaultValue={task.tag}
+						autoComplete="off"
+						list="tags"
+					/>
+					<datalist id="tags">
+						{tags.map(tag => (
+							<option key={tag} value={tag} />
+						))}
+					</datalist>
+				</div>
 
-				<datalist id="tags">
-					{tags.map(tag => (
-						<option key={tag} value={tag} />
-					))}
-				</datalist>
+				<div>
+					<label>Description</label>
+					<textarea name="description" defaultValue={task.description} />
+				</div>
 
-				<label>Description</label>
-				<textarea name="description" defaultValue={task.description} />
-
-				<div className={classes.footer}>
+				<div>
 					<button type="button" onClick={onCancel}>
 						Close
 					</button>
 					<button type="submit">Save</button>
 				</div>
-			</form>
+			</Form>
 		);
 	}
 }
