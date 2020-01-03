f335e86d8b2ecacdb877f42707f4922b162eea6e
gasweld <gasweld@gmail.com>
2020-01-03 20:13:56 +0300

    Styled-components

--- frontend/src/TasksListPage/Tag.js
+++ frontend/src/TasksListPage/Tag.js
@@ -1,6 +1,19 @@
 import React from "react";
-import classes from "./index.css";
 import sha1 from "sha1";
+import styled from "styled-components";
+
+const Container = styled.span`
+	display: inline-block;
+	background-color: ${props => props.color || "#9fa8da"};
+	color: white;
+	font-size: small;
+	font-weight: bold;
+	padding: 0.2em 0.5em;
+	margin-right: 0.5em;
+	border-radius: 4px;
+	flex-grow: 0;
+	flex-basis: auto;
+`;
 
 export default function Tag(props) {
 	const { title, onClick } = props;
@@ -8,12 +21,8 @@ export default function Tag(props) {
 	const color = "#" + sha1(title).substr(0, 6);
 
 	return (
-		<span
-			className={classes.tag}
-			onClick={onClick}
-			style={{ backgroundColor: color }}
-		>
+		<Container color={color} onClick={onClick}>
 			{title}
-		</span>
+		</Container>
 	);
 }
